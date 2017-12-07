fc2fe3e7da1b4afd416d9e371d8b1b45206d0daf
anazahmed <anazahmed93@gmail.com>
2017-12-07 08:30:44 -0800

    Java Code
    Some math operational codes.

---
+++ MathOperations/src/com/digitek/mathoperations/AdditionExamples.java
@@ -0,0 +1,16 @@
+package com.digitek.mathoperations;
+
+public class AdditionExamples {
+	
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		int result = a+b;
+		System.out.println("the result is"+result);
+		
+
+	}
+	
+
+		}
+
---
+++ MathOperations/src/com/digitek/mathoperations/MultiplicationExamples.java
@@ -0,0 +1,14 @@
+package com.digitek.mathoperations;
+
+public class MultiplicationExamples {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 5;
+		int c = 2;
+		int result = b*a*c;
+		System.out.println(result);
+
+	}
+
+}
