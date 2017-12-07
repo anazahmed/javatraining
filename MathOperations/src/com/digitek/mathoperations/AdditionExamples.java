377ac041b910d40ddbe16ab3db609622b31ee91e
anazahmed <anazahmed93@gmail.com>
2017-12-07 10:06:47 -0800

    Added parameteres to method

--- MathOperations/src/com/digitek/mathoperations/AdditionExamples.java
+++ MathOperations/src/com/digitek/mathoperations/AdditionExamples.java
@@ -1,16 +1,50 @@
-package com.digitek.mathoperations;
+package com.digitek.business;
 
 public class AdditionExamples {
 	
 	public static void main(String[] args) {
-		int a = 10;
-		int b = 20;
-		int result = a+b;
-		System.out.println("the result is"+result);
-		
-
+		AdditionExamples ae = new AdditionExamples();
+	int result1 = ae.addTwoNumbers();
+	int result2 = ae.addThreeNumbers();
+	ae.addResults(result1, result2);
+	ae.addDecimals();
 	}
 	
-
+public static int addTwoNumbers() {
+        
+        int a = 10;
+        int b = 20;
+        int result = a+b;
+        System.out.println("result of 2 numbers:" + result);
+        return result;
+                
+    }
+    
+    public static int addThreeNumbers() {
+        
+        int a = 10;
+        int b = 20;
+        int c = 30;
+        int result = a+b+c;
+        System.out.println("result of 3 numbers:" + result);
+        return result;
+                
+    }
+    public static int addResults(int result1,int result2) {
+    	int result = result1 + result2;
+   System.out.println("the result of two results is" + result); 	
+   
+		return result;
+    	
+    }
+    public static double addDecimals() {
+    	
+    	double a = 10.5;
+    	double b = 5.5;
+    	double result = a+b;
+    	System.out.println("The Result is of two decimals is:" + result);
+    	return result;
+    			
+    }
 		}
 
