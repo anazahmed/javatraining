7ddd8137ee6f616edb23dc51e44a948581b51902
anazahmed <anazahmed93@gmail.com>
2017-12-21 08:56:38 -0800

    New Code
    New Examples

---
+++ MathOperations/src/com/digitek/business/AssignmentWithOperators.java
@@ -0,0 +1,23 @@
+package com.digitek.business;
+
+public class AssignmentWithOperators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		   int x = 4;
+	        System.out.println("value of x:"+ x);
+	         
+	    //shortcuts for using binary operators in assignments
+	        x+=4; //x=x+4;
+	        System.out.println("value of x:"+ x);
+	         
+	        x*=4; // x= x*4
+	        System.out.println("value of x:"+ x);
+	 
+	        x%=2; // x = x%2
+	        System.out.println("value of x:"+ x);
+	 
+
+	}
+
+}
