b03cba97657bfe86413ad683aceeb74552170fee
anazahmed <anazahmed93@gmail.com>
2017-12-21 09:16:56 -0800

    New Code
    New Excercise

---
+++ MathOperations/src/com/digitek/business/SwitchOperation.java
@@ -0,0 +1,30 @@
+package com.digitek.business;
+
+public class SwitchOperation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int day =1;
+String daystring;
+switch (day) {
+
+case 1 : daystring = "monday";
+break;
+case 2 : daystring = "Tuesday";
+break;
+case 3 : daystring = "Wednesday";
+break;
+case 4 : daystring = "Thursday";
+break;
+case 5 : daystring = "Friday";
+break;
+case 6 : daystring = "Saturday";
+break;
+case 7 : daystring = "Sunday";
+break;
+default : daystring = "Not a valid day";
+break;
+	}
+System.out.println(daystring);
+}
+}
